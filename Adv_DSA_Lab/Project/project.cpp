49b83f889317f332f63de0f714be0101e06f6475
kakarr0tt0 <oje7w2it@0x676f6b75.anonaddy.me>
2022-12-06 21:46:59 +0530

    End Sem 1 Project

---
+++ Adv_DSA_Lab/Project/project.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cmath>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int main (){
+  srand(time(0)); //Gives Random seed using time in seconds
+  const int size=60;
+  cout << "Enter a letter to begin \n ";
+  char x; 
+  cin>> x;
+  int position = size /2;
+  while (true) {
+    cout << "|START|" ;
+    for (int i=0; i<size;i++) {
+      if (i == position)
+        cout << x;
+      else cout << " ";}
+    cout << "|FINISH|" << endl;
+    int move= 
+    position = position + move;
+    if (position <1) {
+		cout << "You could not finish the race!" <<endl; 
+		break;
+	}
+    if (position >size-1) {
+		cout << "Yay! You finished the race" << endl; 
+		break;
+	}
+    for(int sleep=0; sleep< 1000000 ; ++ sleep);
+  }
+  return 0;
+}
